10a759d420ac981008f9efb44146ae372d524c87
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-09-14 16:52:26 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Skrypt instalacyjny dla Panelu AIO
+
+# Konfiguracja
+PLUGIN_PATH="/usr/lib/enigma2/python/Plugins/Extensions/PanelAIO"
+GIT_USER="OliOli2013"
+GIT_REPO="PanelAIO-Plugin"
+FILES="plugin.py logo.png selection.png" # Dodaj tu inne pliki, jeśli są potrzebne
+
+# Komunikaty
+echo ">>>"
+echo ">>> Instalacja wtyczki Panel AIO..."
+echo ">>>"
+
+# Usuwanie starej wersji
+if [ -d "$PLUGIN_PATH" ]; then
+    echo "> Usuwanie poprzedniej wersji..."
+    rm -rf "$PLUGIN_PATH"
+fi
+
+# Tworzenie katalogu
+mkdir -p "$PLUGIN_PATH"
+
+# Pobieranie plików
+echo "> Pobieranie plików wtyczki..."
+for FILE in $FILES; do
+    wget -q "--no-check-certificate" "https://raw.githubusercontent.com/$GIT_USER/$GIT_REPO/main/$FILE" -O "$PLUGIN_PATH/$FILE"
+done
+
+# Zakończenie
+echo ">>>"
+echo ">>> Instalacja zakończona pomyślnie!"
+echo ">>> Proszę zrestartować Enigma2."
+echo ">>>"
+
+exit 0
