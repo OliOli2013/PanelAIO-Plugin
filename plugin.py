d02c36062296ede0fe89e9487d45678ee98a0c04
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-11-16 17:42:45 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -2,7 +2,7 @@
 """
 Panel AIO
 by Paweł Pawełek | msisystem@t.pl
-Wersja 4.2 - Aktualizacja J00zek repo i poprawki instalatora Ncam
+Wersja 4.2.1 - Poprawka instalatora NCam
 """
 from __future__ import print_function
 from __future__ import absolute_import
@@ -48,7 +48,7 @@ PLUGIN_TMP_PATH = "/tmp/PanelAIO/"
 PLUGIN_ICON_PATH = os.path.join(PLUGIN_PATH, "logo.png")
 PLUGIN_SELECTION_PATH = os.path.join(PLUGIN_PATH, "selection.png")
 PLUGIN_QR_CODE_PATH = os.path.join(PLUGIN_PATH, "Kod_QR_buycoffee.png")
-VER = "4.2"  # <-- Wersja 4.2
+VER = "4.2.1"  # <-- ZMIANA WERSJI (HOTFIX)
 DATE = str(datetime.date.today())
 FOOT = "AIO {} | {} | by Paweł Pawełek | msisystem@t.pl".format(VER, DATE) 
 
@@ -287,7 +287,7 @@ SOFTCAM_AND_PLUGINS_PL = [
     ("Kasuj hasło Oscam", "CMD:CLEAR_OSCAM_PASS"),
     ("oscam.dvbapi - zarządzaj", "CMD:MANAGE_DVBAPI"),
     ("Oscam z Feeda (Auto)", "CMD:INSTALL_BEST_OSCAM"),
-    ("NCam 15.6 (Instalator)", "bash_raw:wget -q --no-check-certificate https://raw.githubusercontent.com/biko-73/Ncam_EMU/main/enigma2-plugin-softcams-ncam-all-images_V15.6-r0_all.ipk -O /tmp/ncam_15.6.ipk && opkg install /tmp/ncam_15.6.ipk && rm -f /tmp/ncam_15.6.ipk"), # <-- POPRAWIONY INSTALATOR
+    ("NCam 15.6 (Instalator)", "bash_raw:wget https://raw.githubusercontent.com/biko-73/Ncam_EMU/main/installer.sh -O - | /bin/sh"), # <-- POPRAWKA v4.2.1
     ("--- Wtyczki Online ---", "SEPARATOR"),
     ("XStreamity - Instalator", "bash_raw:opkg update && opkg install enigma2-plugin-extensions-xstreamity"),
     ("ServiceApp - Instalator", "CMD:INSTALL_SERVICEAPP"),
@@ -309,7 +309,7 @@ SOFTCAM_AND_PLUGINS_EN = [
     ("Clear Oscam Password", "CMD:CLEAR_OSCAM_PASS"),
     ("oscam.dvbapi - manage", "CMD:MANAGE_DVBAPI"),
     ("Oscam from Feed (Auto)", "CMD:INSTALL_BEST_OSCAM"),
-    ("NCam 15.6 (Installer)", "bash_raw:wget -q --no-check-certificate https://raw.githubusercontent.com/biko-73/Ncam_EMU/main/enigma2-plugin-softcams-ncam-all-images_V15.6-r0_all.ipk -O /tmp/ncam_15.6.ipk && opkg install /tmp/ncam_15.6.ipk && rm -f /tmp/ncam_15.6.ipk"), # <-- POPRAWIONY INSTALATOR
+    ("NCam 15.6 (Installer)", "bash_raw:wget https://raw.githubusercontent.com/biko-73/Ncam_EMU/main/installer.sh -O - | /bin/sh"), # <-- POPRAWKA v4.2.1
     ("--- Online Plugins ---", "SEPARATOR"),
     ("XStreamity - Installer", "bash_raw:opkg update && opkg install enigma2-plugin-extensions-xstreamity"),
     ("ServiceApp - Installer", "CMD:INSTALL_SERVICEAPP"),
