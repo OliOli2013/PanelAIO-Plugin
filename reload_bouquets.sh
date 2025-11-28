9b07f9e33d13b9746390926c2eadbb1695806c4d
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-11-29 00:06:18 +0100

    Delete reload_bouquets.sh

--- reload_bouquets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Prosty skrypt do przeładowania listy kanałów (bukietów) w Enigma2
-
-wget -qO - "http://127.0.0.1/web/servicelistreload?mode=0" > /dev/null 2>&1
-wget -qO - "http://127.0.0.1/web/servicelistreload?mode=4" > /dev/null 2>&1
-
-exit 0
