a690706384587138c7142f951915e0c76f9cd123
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-11-29 00:05:47 +0100

    Update update_satellites_xml.sh

--- update_satellites_xml.sh
+++ update_satellites_xml.sh
@@ -5,3 +5,7 @@ echo 'Rozpoczynam pobieranie satellites.xml (0%)...'
 wget -O /etc/tuxbox/satellites.xml http://raw.githubusercontent.com/OpenPLi/tuxbox-xml/master/xml/satellites.xml || { echo 'Błąd: Nie udało się pobrać satellites.xml!'; exit 1; }
 
 echo 'Pobrano i zapisano satellites.xml pomyślnie (100%).'
+sleep 1 # Skrócony sleep
+
+# Po tym skrypcie następuje przeładowanie ustawień w Pythonie, nie restart GUI.
+exit 0
