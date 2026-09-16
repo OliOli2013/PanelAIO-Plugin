377076da055e39441a9ee182bffd61b6b131bea7
Pawel Pawelek <pawelzarzycki61@gmail.com>
2026-09-16 22:34:54 +0200

    Add files via upload

--- data/requested_installers.py
+++ data/requested_installers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Exact publisher commands explicitly requested for the 16.0.1 release."""
 COMMANDS = {
-    'INSTALL_AIOHD_NEXT': 'wget -q --no-check-certificate "https://github.com/OliOli2013/AIOHD-NEXT-Skin/releases/download/3.2.1-r5/enigma2-plugin-skins-aiohd-next_3.2.1-r5_all.ipk" -O /tmp/aiohd-next.ipk && opkg install --force-reinstall /tmp/aiohd-next.ipk && rm -f /tmp/aiohd-next.ipk',
+    'INSTALL_AIOHD_NEXT': 'wget -q --no-check-certificate "https://github.com/OliOli2013/AIOHD-NEXT-Skin/releases/download/3.2.1-r5/enigma2-plugin-skins-aiohd-next_3.2.1-r5_all.ipk" -O /tmp/aiohd-next.ipk && opkg --force-reinstall install /tmp/aiohd-next.ipk && rm -f /tmp/aiohd-next.ipk',
     'INSTALL_FURY_SKIN': 'wget -q "--no-check-certificate" https://raw.githubusercontent.com/islam-2412/FuryBiss/refs/heads/main/fury/installer.sh -O - | /bin/sh',
     'INSTALL_JIHAD_SKIN': 'wget -q "--no-check-certificate" https://raw.githubusercontent.com/ahmeds200917/A.Shawky/refs/heads/main/install_jihad.sh -O - | /bin/sh',
     'INSTALL_ADULT_XXX': 'wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/xxxplugin/main/installer.sh -O - | /bin/sh',
